afce9810ff7956477ee91fc8cc5c7c5e3d526527
zachvenables <54538516+zachvenables@users.noreply.github.com>
2023-10-01 10:24:14 -0400

    working site

--- pages/index.js
+++ pages/index.js
@@ -2,62 +2,48 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+
+  const showBirthDate= () => {
+    alert("It's Oct 13th")
+  }
+
+  const listItem = (content, anchor) => {
+    return (
+      <li className={styles.listitem}>{content}<a className={styles.anchor} href={anchor}>Click Here</a></li>
+
+    );
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Zack's Birthday Stuff</title>
+        <link rel="icon" href="/birthday-cake.png" />
       </Head>
 
       <main>
+        <img src="/birthday-cake.png"></img>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Stuff for <a href="#" onClick={() => showBirthDate()}>Zack's birthday!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h2 className={styles.description}>
+          Here is a comprehensive list of things you can consider buying Zack for his birthday:
+        </h2>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <ul className={styles.list}>
+          {listItem("Bike Headlight: ", "https://www.amazon.com/gp/product/B07H26DJSR/?smid=A26A9QLFNQPSCL&tag=thewire06-20")}
+          {listItem("Cycling Shoes, size 12.5, Black/Silver/Gum: ", "https://www.competitivecyclist.com/crank-brothers-stamp-lace-cycling-shoe-mens?CMP_SKU=CRNH013&MER=0406&utm_source=Avantlink&utm_medium=Affiliate&CSPID=0920&avad=246073_a3399b005&SharedId=246073_a3399b005&utm_medium=Affiliate&utm_source=www.toptenreviews.com&utm_campaign=datafeed&utm_term=Content")}
+          {listItem("Bike seat cover: ", "https://www.amazon.com/dp/B01H71AZ36?linkCode=ogi&tag=bicycling-auto-20&ascsubtag=%5Bartid%7C2143.g.40709971%5Bsrc%7Cwww.google.com%5Bch%7C%5Blt%7Cpsv%5Bpid%7Caab76536-c19f-4b49-aa5b-cde3c55f2786")}
+          {listItem("Cycling Jacket, size M:  ", "https://www.decathlon.com/collections/bike-jackets/products/van-rysel-cold-weather-lightweight-road-cycling-jacket?avad=55097_c3399427d&variant=32498323030078")}
+          {listItem("Headband, size S, black: ", "https://www.amazon.com/dp/B09991Z9JC?linkCode=ogi&tag=bicycling-auto-20&ascsubtag=%5Bartid%7C2143.g.20011461%5Bsrc%7Cwww.google.com%5Bch%7C%5Blt%7Csale%5Bpid%7Cd8081d73-7e58-40e0-b900-d6c705d7c40f&th=1&psc=1")}
+        </ul>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <p>Ok, that's it. Why are you still here?</p>
       </footer>
 
       <style jsx>{`
